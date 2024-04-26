9a00c3b0e22d04f8fbe845e2c2e22d3d41f2f0b4
awllms <antcodess@gmail.com>
2024-04-26 12:54:56 -0700

    Adds initial vercel deployment information

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const cors = require('cors');
-const dotenv = require('dotenv');
+const cors = require("cors");
+const dotenv = require("dotenv");
 const { Configuration, OpenAIApi } = require("openai");
 
 dotenv.config();
@@ -14,52 +14,55 @@ const MAXTOKENS = 200;
 
 const port = process.env.PORT || 3001;
 
+const environment = process.env.NODE_ENV ? "production" : "dev";
+
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-app.get('*', (req, res) => {
-    res.redirect(301, 'https://awllms.com');
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.post('/ai', async (req, res) => {
-    const {data, model} = req.body;
-    data["max_tokens"] = MAXTOKENS;
+app.post("/ai", async (req, res) => {
+  const { data, model } = req.body;
+  data["max_tokens"] = MAXTOKENS;
 
-    try {
-        const response = await openai.createCompletion(model, data);
-        console.log(response.data);
+  try {
+    const response = await openai.createCompletion(model, data);
+    console.log(response.data);
 
-        res.status(200).send(response.data);
-    } catch (error) {
+    res.status(200).send(response.data);
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
 
-        if (error.response) {
-          console.log(error.response.status);
-          console.log(error.response.data);
+      res.status(400).send(error.response.data);
+    } else {
+      console.log(error.message);
 
-          res.status(400).send(error.response.data);
-
-        } else {
-          console.log(error.message);
-
-          res.status(500).send(error.message);
-        }
+      res.status(500).send(error.message);
     }
+  }
 });
 
-const server = app.listen(port, (error) => {
-    if (error) throw error;
-    console.log(
-      `App is running in ${process.env.NODE_ENV} on port ${
-        process.env.PORT ? process.env.PORT : 3001
-      }`
-    );
+app.get("*", (req, res) => {
+  res.redirect(301, "https://awllms.com");
 });
-  
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
-    server.close(() => process.exit(1));
 
-});
+// app.listen(port, () => {
+//   console.log(`App is running in ${environment} on port ${port}`);
+// });
+
+// const server = app.listen(port, (error) => {
+//   if (error) throw error;
+//   console.log(`App is running in ${environment} on port ${port}`);
+// });
+
+// process.on("unhandledRejection", (err, promise) => {
+//   console.log(`Error: ${err.message}`);
+//   server.close(() => process.exit(1));
+// });
+
+module.exports = app;
