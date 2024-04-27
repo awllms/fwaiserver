7ad92ad9c3973feb6aa9122fb53dec5919d1afdd
awllms <antcodess@gmail.com>
2024-04-26 17:36:26 -0700

    Updates server to work correctly with new openai api

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { Configuration, OpenAIApi } = require("openai");
+// const { Configuration, OpenAIApi } = require("openai");
+const OpenAI = require("openai");
 
 dotenv.config();
 
 const app = express();
 
-// const corsOptions = {
-//   origin: ["http://localhost:3000", "http://awllms.com", "https://awllms.com"],
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   optionsSuccessStatus: 204,
-//   credentials: true,
-// };
-app.options("*", cors());
-app.use(cors({ origin: "*", credentials: true }));
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 
+// app.use(function (err, req, res, next) {
+//   console.error(err.stack);
+//   res.status(500).send("Something broke!");
+// });
+
 // app.use((req, res, next) => {
-//   console.log(
-//     "Received request:",
-//     req.method,
-//     req.path,
-//     "from origin",
-//     req.headers.origin
-//   );
+//   console.log(`Incoming ${req.method} request to ${req.url}`);
+//   console.log(`Headers: ${JSON.stringify(req.headers)}`);
 //   next();
+// });
+
+// app.use((err, req, res, next) => {
+//   console.error(`Error processing request: ${err.message}`);
+//   res.status(500).send("Internal Server Error");
 // });
 
 const MAXTOKENS = 200;
@@ -34,34 +42,31 @@ const port = process.env.PORT || 3001;
 
 const environment = process.env.NODE_ENV ? "production" : "dev";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(configuration);
-// const openai = new OpenAI();
+// const openai = new OpenAIApi(configuration);
+const openai = new OpenAI();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.post("/ai", async (req, res) => {
-  req.headers.authorization = process.env.OPENAI_API_KEY;
+  // req.headers.authorization = process.env.OPENAI_API_KEY;
   const { data, model } = req.body;
-  data["max_tokens"] = MAXTOKENS;
-
-  // const completion = await openai.chat.completions.create({
-  //   ...data,
-  //   model: model,
-  // });
 
   try {
-    // const response = await openai.createCompletion(model, data);
-    const response = await openai.createCompletion({
-      ...data,
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: data.prompt },
+      ],
       model: model,
     });
-    console.log(response.data);
 
-    res.status(200).send(response.data);
+    console.log(response.choices[0].message);
+
+    res.status(200).send(response);
   } catch (error) {
     if (error.response) {
       console.log(error.response.status);
@@ -80,9 +85,9 @@ app.get("*", (req, res) => {
   res.redirect(301, "https://awllms.com");
 });
 
-// app.listen(port, () => {
-//   console.log(`App is running in ${environment} on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`App is running in ${environment} on port ${port}`);
+});
 
 // const server = app.listen(port, (error) => {
 //   if (error) throw error;
